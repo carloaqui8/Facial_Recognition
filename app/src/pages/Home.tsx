3c768213d2418ad062359ea81b3039ee1b6cba6e
carloaqui8 <cpaqui@ucsc.edu>
2024-12-29 16:37:09 -0800

    camera

---
+++ app/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Webcam from 'react-webcam';
+
+function Home() {
+    return (
+        <div className='header'>
+            <Webcam />
+        </div>
+    );
+}
+
+export default Home;
